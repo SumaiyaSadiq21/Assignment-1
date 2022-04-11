e4b390d6f673f062e5a36863ebc809135dea7b2e
FarhanaBinteHakimRimi <96817621+FarhanaBinteHakimRimi@users.noreply.github.com>
2022-04-12 00:44:44 +0600

    Add files via upload

---
+++ Student/createStudent.php
@@ -0,0 +1,27 @@
+<h1>Create Student</h1>
+
+
+
+<form method=get action=Insertstudent.php>
+
+
+	dept: <input type=text name=dept> <br>
+
+	<p>
+	name: <input type=text name=name> <br>
+
+	<p>
+	nid: <input type=text name=nid> <br>
+
+	<p>
+	birth: <input type=date name=birth> <br>
+
+	<p>
+	address: <input type=text name=address> <br>
+
+	<p>
+	
+
+	<input type=submit value=Insert>
+
+</form>
---
+++ Student/update_result1.php
@@ -0,0 +1,17 @@
+<?php
+	$id = $_GET["id"];    $dept = $_GET["dept"];	$name = $_GET["name"];	$nid = $_GET["nid"];	$birth = $_GET["birth"];	$address = $_GET["address"];
+
+	require_once('db_connect.php');
+	$connect = mysqli_connect( HOST, USER, PASS, DB )
+		or die("Can not connect");
+
+	$query 	= "UPDATE student SET dept='$dept', name='$name', nid='$nid', birth='$birth', address='$address' WHERE id = $id";
+	echo $query;
+
+	mysqli_query( $connect, $query )
+		or die("Can not execute query");
+
+	echo "<p>Record updated:<br> dept = $dept <br> name = $name <br> nid = $nid <br> birth = $birth <br> address = $address ";
+
+	echo "<p><a href=readStudent.php>READ all records</a>";
+?>
