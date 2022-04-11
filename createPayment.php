e5e06af88343a36caf97cfd2a730f9b52025b06c
Sumaiya Sadiq <sumaiyasadiq72@gmail.com>
2022-04-12 00:10:35 +0600

    first commit

---
+++ createPayment.php
@@ -0,0 +1,26 @@
+<h1>Create Payment</h1>
+
+
+
+<form method=get action=paymentresult.php>
+
+
+	payment_id: <input type=text name=payment_id> <br>
+
+	<p>
+	student_id: <input type=text name=student_id> <br>
+
+	<p>
+	amount: <input type=text name=amount> <br>
+
+	<p>
+	date: <input type=date name=date> <br>
+
+	<p>
+
+	<p>
+	
+
+	<input type=submit value=Insert>
+
+</form>
