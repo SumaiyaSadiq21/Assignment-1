cebf9536c7a1229fe5c3981251e16e8f41a37c8e
Sumaiya Binte Sadiq <96006910+SumaiyaSadiq21@users.noreply.github.com>
2022-04-12 02:56:47 +0600

    Delete update_result.php

--- update_result.php
+++
@@ -1,17 +0,0 @@
-<?php
-	$payment_id = $_GET["payment_id"];	$student_id = $_GET["student_id"];	$amount: = $_GET["amount:"];	$date = $_GET["date"];
-
-	require_once('db_connect.php');
-	$connect = mysqli_connect( HOST, USER, PASS, DB )
-		or die("Can not connect");
-
-	$query 	= "UPDATE payment SET student_id='$student_id', amount='$amount', date='$date' WHERE payment_id = $payment_id";
-	echo $query;
-
-	mysqli_query( $connect, $query )
-		or die("Can not execute query");
-
-	echo "<p>Record updated:<br> student_id = $student_id <br> amount = $amount <br> date = $date ";
-
-	echo "<p><a href=readPayment.php>READ all records</a>";
-?>
